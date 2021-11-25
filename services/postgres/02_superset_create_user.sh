931d87d13734fe953de970c27beb927efa68e1db
Alison Mukoma <alison.mukoma@trivago.com>
2021-11-26 00:16:04 +0100

    Update stack

--- services/postgres/02_superset_create_user.sh
+++ services/postgres/02_superset_create_user.sh
@@ -4,9 +4,9 @@ set -e # exit if a command exits with a not-zero exit code
 POSTGRES="psql -U postgres"
 
 # create a role for superset
-echo "Creating database role: superset"
+echo "Creating database role: digipro"
 $POSTGRES <<-EOSQL
-CREATE USER superset WITH
+CREATE USER digipro WITH
     LOGIN
     NOSUPERUSER
     NOCREATEDB
--- services/postgres/03_superset_create_db.sh
+++ services/postgres/03_superset_create_db.sh
@@ -4,7 +4,7 @@ set -e # exit immediately if a command exits with a non-zero status.
 POSTGRES="psql --username postgres"
 
 # create database for superset
-echo "Creating database: superset"
+echo "Creating database: digipro"
 $POSTGRES <<EOSQL
-CREATE DATABASE superset OWNER superset;
+CREATE DATABASE digipro OWNER digipro;
 EOSQL
